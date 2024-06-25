9ae24c6acc6c1777ae29473eb6a9201304f1e5c9
Alexa Der <alexajasmine.der333@gmail.com>
2024-06-25 10:10:55 -0700

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Alexa Der on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
